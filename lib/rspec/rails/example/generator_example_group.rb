269700334cf99c3dd5e81553d7814de1333f0e5b
Pablo Herrero <pablodherrero@gmail.com>
2013-04-04 23:53:41 -0300

    Add initializations for Generators Example Groups

--- lib/rspec/rails/example/generator_example_group.rb
+++ lib/rspec/rails/example/generator_example_group.rb
@@ -10,20 +10,34 @@ else
   require 'rails_generators_test_case_modules'
 end
 
-
 module RSpec
   module Rails
     module GeneratorExampleGroup
       extend ActiveSupport::Concern
 
-      include ::Rails::Generators::Testing::Behaviour
-      include ::Rails::Generators::Testing::SetupAndTeardown
       include ::Rails::Generators::Testing::Assertions
       include FileUtils
 
       include RSpec::Rails::GeneratorMatchers
 
       ::Rails::Generators.no_color!
+
+      included do
+        include ::Rails::Generators::Testing::Behaviour
+
+        metadata[:type] = :generator
+
+        tests described_class
+
+        prepend_before do
+          destination_root_is_set?
+          ensure_current_path
+        end
+
+        append_after do
+          ensure_current_path
+        end
+      end
 
     end
   end
